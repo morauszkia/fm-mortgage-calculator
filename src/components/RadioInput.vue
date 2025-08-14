7c9d078cc905a43d9ba6fcacb275e835b191ffeb
mantis <morauszki.andras@gmail.com>
2025-08-14 13:52:57 +0200

    Custom RadioInput component

---
+++ src/components/RadioInput.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+interface Props {
+  id: string;
+  title: string;
+  options: { id: string; label: string }[];
+}
+
+const { id, title, options } = defineProps<Props>();
+</script>
+
+<template>
+  <div>
+    <label :for="id">{{ title }}</label>
+    <div class="option" v-for="option in options">
+      <input type="radio" :name="id" :id="option.id" /><label for="repayment">{{
+        option.label
+      }}</label>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
