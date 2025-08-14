1fff85c6f25cc97da8c91e5e20be3e3c03bfbe52
mantis <morauszki.andras@gmail.com>
2025-08-14 13:17:00 +0200

    Export form and output to custom components

---
+++ src/components/Form.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup></script>
+
+<template>
+  <form>
+    <h1>Mortgage Calculator</h1>
+    <button type="reset">Clear All</button>
+    <div>
+      <label for="amount">Mortgage Amount</label
+      ><input type="number" id="amount" />
+    </div>
+    <div>
+      <label for="term">Mortgage Term</label><input type="number" id="term" />
+    </div>
+    <div>
+      <label for="rate">Interest Rate</label><input type="number" id="rate" />
+    </div>
+    <div>
+      <label for="type">Mortgage Type</label>
+      <input type="radio" name="type" id="repayment" /><label for="repayment"
+        >Repayment</label
+      >
+      <input type="radio" name="type" id="interest" /><label for="interest"
+        >Interest Only</label
+      >
+    </div>
+    <button type="submit">Calculate Repayments</button>
+  </form>
+</template>
+
+<style scoped></style>
---
+++ src/components/Output.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section>
+    <!-- Empty results -->
+    <h2>Results shown here</h2>
+    <p>
+      Complete the form and click “calculate repayments” to see what your
+      monthly repayments would be.
+    </p>
+
+    <!-- Completed results -->
+    <h2>Your results</h2>
+    <p>
+      Your results are shown below based on the information you provided. To
+      adjust the results, edit the form and click “calculate repayments” again.
+    </p>
+    <div>
+      <p>Your monthly repayments</p>
+      <p>&pound;1,797.74</p>
+    </div>
+    <div>
+      <p>Total you'll repay over the term</p>
+      <p>&pound;539,322.94</p>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
