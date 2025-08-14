12856d104ac55fcf1c553390d7fbd04ed0fb1f22
mantis <morauszki.andras@gmail.com>
2025-08-14 13:53:22 +0200

    Replace raw html with custom components

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,29 +1,65 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import SubmitButton from "./SubmitButton.vue";
+import ClearButton from "./ClearButton.vue";
+import FormInput from "./FormInput.vue";
+import RadioInput from "./RadioInput.vue";
+
+type InputData = {
+  id: string;
+  label: string;
+  unit: string;
+  wide?: boolean;
+};
+
+type RadioOption = {
+  id: string;
+  label: string;
+};
+
+const inputs: InputData[] = [
+  {
+    id: "amount",
+    label: "Mortgage Amount",
+    unit: "£",
+    wide: true,
+  },
+  {
+    id: "term",
+    label: "Mortgage Term",
+    unit: "years",
+  },
+  {
+    id: "rate",
+    label: "Interest Rate",
+    unit: "%",
+  },
+];
+
+const radioOptions: RadioOption[] = [
+  {
+    id: "repayment",
+    label: "Repayment",
+  },
+  {
+    id: "interest",
+    label: "Interest Only",
+  },
+];
+</script>
 
 <template>
   <form>
     <h1>Mortgage Calculator</h1>
-    <button type="reset">Clear All</button>
-    <div>
-      <label for="amount">Mortgage Amount</label
-      ><input type="number" id="amount" />
-    </div>
-    <div>
-      <label for="term">Mortgage Term</label><input type="number" id="term" />
-    </div>
-    <div>
-      <label for="rate">Interest Rate</label><input type="number" id="rate" />
-    </div>
-    <div>
-      <label for="type">Mortgage Type</label>
-      <input type="radio" name="type" id="repayment" /><label for="repayment"
-        >Repayment</label
-      >
-      <input type="radio" name="type" id="interest" /><label for="interest"
-        >Interest Only</label
-      >
-    </div>
-    <button type="submit">Calculate Repayments</button>
+    <ClearButton />
+    <FormInput
+      v-for="input in inputs"
+      :id="input.id"
+      :label="input.label"
+      :unit="input.unit"
+      :wide="input.wide"
+    />
+    <RadioInput id="type" title="Mortgage Type" :options="radioOptions" />
+    <SubmitButton />
   </form>
 </template>
 
