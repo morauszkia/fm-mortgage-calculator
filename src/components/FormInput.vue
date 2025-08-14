95a303c79e58a52fc96a94954dcfab7314fb3933
mantis <morauszki.andras@gmail.com>
2025-08-14 13:42:59 +0200

    FormInput custom component

---
+++ src/components/FormInput.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+interface Props {
+  id: string;
+  label: string;
+  unit: string;
+  wide?: boolean;
+}
+
+const { id, label, unit, wide = false } = defineProps<Props>();
+</script>
+
+<template>
+  <div :class="`form-control ${wide ? 'wide' : ''}`" >
+    <label :for="id">{{ label }}</label>
+    <div class="input">
+      <p :class="`unit ${unit === '&pound;' ? 'start' : ''}`">{{ unit }}</p>
+      <input type="number" :id />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
